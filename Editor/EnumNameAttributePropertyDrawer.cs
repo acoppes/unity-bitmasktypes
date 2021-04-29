fb8851197b5bfd4870edb2d5fdf4de75c3b494fb
Ariel Coppes <ariel.coppes@gmail.com>
2021-04-29 15:00:57 -0300

    same idea but now reading the name from assets

--- Editor/EnumNameAttributePropertyDrawer.cs
+++ Editor/EnumNameAttributePropertyDrawer.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,55 +6,35 @@ namespace Gemserk.BitmaskTypes.Editor
     [CustomPropertyDrawer(typeof(EnumNameAttribute))]
     public class EnumNameAttributePropertyDrawer : PropertyDrawer
     {
-        public struct TypeName
-        {
-            public int type;
-            public string name;
-
-            public TypeName(int t, string n)
-            {
-                type = t;
-                name = n;
-            }
-        }
-        
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             var enumNameAttribute = attribute as EnumNameAttribute;
 
-            var names = new TypeName[]
+            var assets = AssetDatabase.FindAssets($"t:{typeof(EnumNameTypeAsset).Name} {enumNameAttribute.namesAsset}");
+            if (assets.Length == 0)
             {
-                new TypeName(1 << 2, "Second"),
-                new TypeName(1 << 3, "Third"),
-                new TypeName(1 << 1, "First"),
-            };
-            
-            // find asset 
+                EditorGUI.PropertyField(position, property, label);
+                return;
+            }
 
-            var namesList = names.ToList();
-            namesList.Sort((a, b) => a.type.CompareTo(b.type));
-            var newNames = namesList.Select(n => n.name).ToArray();
+            var typeAsset = AssetDatabase.LoadAssetAtPath<EnumNameTypeAsset>(AssetDatabase.GUIDToAssetPath(assets[0]));
+
+            var names = typeAsset.GetOrderedNames();
+
+            if (names.Length == 0)
+            {
+                EditorGUI.PropertyField(position, property, label);
+                return;
+            }
 
             EditorGUI.BeginChangeCheck();
-            var newValue = EditorGUI.MaskField(position, label, property.intValue, newNames);
+            var newValue = EditorGUI.MaskField(position, label, property.intValue, names);
 
             if (EditorGUI.EndChangeCheck())
             {
                 property.intValue = newValue;
             }
-
-            // EditorGUI.PropertyField(position, property, label);
         }
-
-        // public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-        // {
-        //     return base.GetPropertyHeight(property, label);
-        // }
-
-        // public override VisualElement CreatePropertyGUI(SerializedProperty property)
-        // {
-        //     return base.CreatePropertyGUI(property);
-        // }
 
         public override bool CanCacheInspectorGUI(SerializedProperty property)
         {
