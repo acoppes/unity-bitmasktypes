51a1b16dcf203a2c9712cb4a7d53ca4ada088804
Ariel Coppes <ariel.coppes@gmail.com>
2023-08-26 20:16:07 -0300

    just testing valid state names

--- Assets/Gemserk.ExamplesCore/Damages.Generated.cs
+++ Assets/Gemserk.ExamplesCore/Damages.Generated.cs
@@ -23,12 +23,15 @@ namespace Gemserk.Examples
         
         public static int Poison = 1 << 3;
         
+        public static int Ranged_Fire = 1 << 4;
+        
         public static string ValueToName(int value)
         {
             if (value == Melee) return nameof(Melee);
             if (value == Ranged) return nameof(Ranged);
             if (value == Thorn) return nameof(Thorn);
             if (value == Poison) return nameof(Poison);
+            if (value == Ranged_Fire) return nameof(Ranged_Fire);
             return null;
         }
         
@@ -38,6 +41,7 @@ namespace Gemserk.Examples
             if ((mask & Ranged) == Ranged) collection.Add(nameof(Ranged));
             if ((mask & Thorn) == Thorn) collection.Add(nameof(Thorn));
             if ((mask & Poison) == Poison) collection.Add(nameof(Poison));
+            if ((mask & Ranged_Fire) == Ranged_Fire) collection.Add(nameof(Ranged_Fire));
         }
     }
 }
